16e8b3209fe5ea9a3fe5b0abc10abb99a75acbe7
Agência Margaretas <contato@agenciamargaretas.com>
2025-03-11 11:49:45 -0300

    feat: adiciona seção de portfólio inspirada no Digtek

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSlider from '@/components/sections/HeroSlider'
 import ServicesSection from '@/components/sections/ServicesSection'
+import PortfolioSection from '@/components/sections/PortfolioSection'
 
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col">
       <HeroSlider />
       <ServicesSection />
+      <PortfolioSection />
     </main>
   )
 } 
