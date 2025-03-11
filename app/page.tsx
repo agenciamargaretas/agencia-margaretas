b96cb1bf5ba554a99e0ad47fe651fd1025b322c9
Agência Margaretas <contato@agenciamargaretas.com>
2025-03-11 16:25:17 -0300

    fix: corrige importações na página principal

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import PortfolioSection from '@/components/sections/PortfolioSection'
-import AgencyValueSection from '@/components/sections/AgencyValueSection'
-import HeroSection from '@/components/sections/HeroSection'
-import ContactSection from '@/components/sections/ContactSection'
+import PortfolioSection from '../components/sections/PortfolioSection'
+import AgencyValueSection from '../components/sections/AgencyValueSection'
+import HeroSection from '../components/sections/HeroSection'
+import ContactSection from '../components/sections/ContactSection'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col">
+    <main>
       <HeroSection />
       <AgencyValueSection />
       <PortfolioSection />
